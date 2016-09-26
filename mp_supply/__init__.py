3a3808eefade775b722055fcfb05796295b6c4d9
andrewidya <andrywidyaputra@gmail.com>
2016-09-26 22:45:57 +0700

    cleaning unneeded files

--- mp_supply/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'mp_supply.apps.ManPowerSupplyConfig'
--- mp_supply/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ManPowerSupplyConfig(AppConfig):
-	name = 'mp_supply'
-	verbose_name = 'Man Power Supply Application'
