65d32cfdd8af742e5b7d5e0a8cf9100d8206c01b
andrewidya <andrywidyaputra@gmail.com>
2016-10-21 17:44:27 +0700

    Reporting Module Added

--- django_reporting/utils.py
+++ django_reporting/utils.py
@@ -4,17 +4,23 @@ from cStringIO import StringIO
 from z3c.rml import rml2pdf
 
 class Reporting(object):
+	template_name = ''
+	context = None
 	def __init__(self, context, template_name=None, output=None):
-		if template_name is None:
+		if template_name is not None:
+			self.template_name = template_name
+
+		if self.template_name is None:
 			raise "template_name parameter must be not None"
-		self.template_name = template_name
+
 		if isinstance(context, Context):
 			self.context = context
 		else:
 			raise "context parameter must be instance of Context class"
+
 		if output is not None:
 			self.output_file = output
-		else:
+		if self.output_file is None:
 			self.output_file = "default.pdf"
 
 	def get_template(self):
@@ -22,18 +28,20 @@ class Reporting(object):
 		return template
 
 	def get_context_data(self):
-		return self.context
+		context = self.context
+		context['filename'] = self.output_file
+		return context
 
 	def render(self):
 		template = self.get_template()
 		rml = template.render(self.get_context_data())
-		#rml.encode('utf-8')
+		rml.encode('utf-8')
 		#buf = StringIO()
 		data = rml2pdf.parseString(rml)
 		#buf.reset()
 		#pdf_data = buf.read()
 		response = HttpResponse(content_type='application/pdf')
 		response.write(data.read())
-		response['Content-Disposition'] = 'attachment; filename="default.pdf"'
+		response['Content-Disposition'] = 'attachment; filename="{0}"'.format(self.output_file)
 		return response
 
--- hrm/views.py
+++ hrm/views.py
@@ -7,7 +7,7 @@ import os
 
 def employee_report(request):
 	employees = Employee.objects.all()
-	template = os.getcwd() + '/hrm/template_reports/employee.rml'
+	template = os.getcwd() + '/hrm/template_reports/employee_2.rml'
 	context = Context({'employees': employees})
 	report = Reporting(context, template_name=template, output="employee.pdf")
 	return report.render()
