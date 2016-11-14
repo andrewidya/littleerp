455cc1083aba7a7ecea19f5f90279f24b5cdcc49
andrewidya <andrywidyaputra@gmail.com>
2016-11-14 23:17:02 +0700

    Editing payroll procedure

--- django_adminlte/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoAdminLTEConfig(AppConfig):
-    name = 'django_adminlte'
-    verbose_name = 'Django AdminLTE Theme'
-
-    def ready(self):
-        pass
--- django_adminlte/settings.py
+++
@@ -1,3 +0,0 @@
-from django.conf import settings
-
-ADMIN_TITLE = getattr(settings, "ADMINLTE_ADMIN_TITLE", 'Django AdminLTE')
